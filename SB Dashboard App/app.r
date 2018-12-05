0146b6382fdd866c5c5932d3e0ffa866c9af894c
Ashley Klesmit <42552977+aklesmit@users.noreply.github.com>
2018-12-04 22:06:48 -0600

    Update app.r

--- SB Dashboard App/app.r
+++ SB Dashboard App/app.r
@@ -2,7 +2,7 @@ library(leaflet)
 library(rgeos)
 library(rgdal)
 
-
+#Load data sets
 schoolsSpatial <- readOGR(dsn = "C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject", layer = "School_Boundaries")
 
 parksPoints <- read.csv("C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject/Parks_Locations_and_Features.csv", stringsAsFactors = F)
@@ -10,12 +10,30 @@ parksPoints <- read.csv("C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/Fina
 parksSpatial <- SpatialPointsDataFrame(coords = parksPoints[,c("Lon","Lat")], data = parksPoints,
                                        proj4string = CRS("+proj=longlat +datum=WGS84"))
 
+AbandonedPropertyParcels <- readOGR(dsn = "../datasets/Abandoned_Property_Parcels/", layer = "Abandoned_Property_Parcels", stringsAsFactors = FALSE)
+CityCouncilDistricts <- readOGR(dsn = "../datasets/City_Council_Districts/", layer = "City_Council_Districts", stringsAsFactors = FALSE)
+AbandonedPropertyParcels.center <- SpatialPointsDataFrame(gCentroid(AbandonedPropertyParcels, byid=TRUE), 
+                                                          AbandonedPropertyParcels@data, match.ID=FALSE)
+
+#Create pop ups
 parksSpatial$popup <- paste("<b>",parksSpatial$Park_Name,"</b><br>",
                             "Type: ",parksSpatial$Park_Type,"<br>",
                             "Address: ",parksSpatial$Address,sep ="")
 
-pal <- colorFactor(palette = 'Set1', domain = parksSpatial$Park_Type)
+AbandonedPropertyParcels$popup <- paste("<b>",AbandonedPropertyParcels$Outcome_St,"</b><br>",
+                            "Address: ", AbandonedPropertyParcels$Address_Nu, AbandonedPropertyParcels$Street_Nam,AbandonedPropertyParcels$Suffix, "<br>", 
+                            "Structure Type: ", AbandonedPropertyParcels$Structures, "<br>", 
+                            "District: ", AbandonedPropertyParcels$Council_Di)
 
+CityCouncilDistricts$popupCouncils <- paste("<b>","District: ", CityCouncilDistricts$OBJECTID, "</b><br>",
+                                            "Council Member: ", CityCouncilDistricts$Council_Me)
+
+#Set color palettes
+pal <- colorFactor(palette = 'Set1', domain = parksSpatial$Park_Type)
+pal_A <- colorFactor(palette = "Accent", domain =AbandonedPropertyParcels$Outcome_St)
+
+
+#Create UI
 ui <- fluidPage(
   
   # Application title
@@ -31,8 +49,31 @@ ui <- fluidPage(
              ),
              tabPanel("Doug"
              ),
-             tabPanel("Ashley"
+             
+             #START ASHLEY
+             tabPanel("Abandoned Properties",
+                      titlePanel("Abandoned Properties"),
+                      "This tab shows the abandoned properties in South Bend. The map shows the city of South Bend 
+                      with each of the Districts plotted and highlighted in gray. When clicking on a gray area, a
+                      popup will appear with the district number and the district's council member. Additionally, 
+                      each abandoned property's property lines are mapped and are color-coded according to their
+                      outcome status: Deconstructed, Demolished, Occupied & Not Repaired, Repaired, and 
+                      Repaired & Occupied. When clicking on a property, a popup will appear with more information
+                      about each property.",
+                      br(),br(),
+                      "The bar graph below the map shows the count of each property's status broken out by district. This
+                      is a quick way to compare the number of abandoned properties in each district. This graph is also
+                      color-coded the same way that the map is.",
+                      br(),br(),
+                      tabsetPanel(
+                        #show a map of the abandoned properties
+                        tabPanel("Map", h3("Map of Abandoned Properties in South Bend"), leafletOutput("AbandonedPropertyParcels", height = 600, width = "90%")), 
+                        #show a bar graph of the abandoned properties by district
+                        tabPanel("Bar Graph", h3("Bar Graph of Abandoned Properties by District"), plotOutput("plot_abandonedbydistrict", click = "plot_click", height = 600, width = "90%"))
+                      )
              ),
+             #END ASHLEY
+             
              tabPanel("Udai"
              ),
              tabPanel("Sam"
@@ -59,6 +100,30 @@ server <- function(input, output) {
       addLegend(pal = pal, values = parksSpatial$Park_Type) %>%
       addPolygons(data = schoolsSpatial, color = "blue", weight = 2, opacity = 0.6, popup = ~School)  
   })
+  
+  #create leaflet map - ASHLEY
+  output$AbandonedPropertyParcels <-
+      renderLeaflet({
+      leaflet()  %>%
+      addTiles()  %>%
+      addProviderTiles(providers$OpenStreetMap) %>% #sets map design
+      addPolygons(data = CityCouncilDistricts, popup = ~popupCouncils, fillColor = ~pal_A(Dist), #creates district lines
+                  weight = 2,
+                  opacity = 1,
+                  color = "white") %>% 
+      addPolylines(data = AbandonedPropertyParcels, popup = ~popup, color = ~pal_A(Outcome_St), fillOpacity = 1)
+  })
+  
+  #create bar graph - ASHLEY
+  output$plot_abandonedbydistrict <- renderPlot({
+    ggplot(data = (AbandonedPropertyParcels@data), aes(fill = (AbandonedPropertyParcels@data)$Outcome_St, #color by outcome
+                                                       x = (AbandonedPropertyParcels@data)$Council_Di)) + 
+      geom_bar() + #make a bar graph
+      labs(fill = "Outcome", x = "District", y = "Count") + #rename the axis and legend
+      theme_bw() + #make a clean theme
+      scale_fill_brewer(palette="Accent", na.value="grey") #same palette as palette for map
+  })
+  
 }
 
 # Run the application 
