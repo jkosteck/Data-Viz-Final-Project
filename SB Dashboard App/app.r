8f4991ad2cce3040bb2a1a4776ae80589c8c4883
jkosteck <42282134+jkosteck@users.noreply.github.com>
2018-12-08 17:07:10 -0600

    Email Updates
    Cleaned-up file loading
    
    Added functional email tab.

--- SB Dashboard App/app.r
+++ SB Dashboard App/app.r
@@ -7,26 +7,46 @@ library(raster)
 library(rgeos)
 library(ggplot2)
 library(tidyverse)
+library(rJava)
+library(mailR)
+library(shinyAce)
 
-#Load data sets
-schoolsSpatial <- readOGR(dsn = "C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject", layer = "School_Boundaries")
+########### Load data sets ###########
+#School data
+schoolsSpatial <- readOGR(dsn = "C:/Users/kost1/Documents/GitHub/data-Viz-Final-Project/data/School_Boundaries",
+                          layer = "School_Boundaries")
 
-parksPoints <- read.csv("C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject/Parks_Locations_and_Features.csv", stringsAsFactors = F)
+#Park data
+parksPoints <- read.csv("C:/Users/kost1/Documents/GitHub/data-Viz-Final-Project/data/Parks_Locations_and_Features.csv",
+                        stringsAsFactors = F)
+
+#Abandoned Property data
+AbandonedPropertyParcels <- readOGR(dsn = "C:/Users/kost1/Documents/GitHub/data-Viz-Final-Project/data/Abandoned_Property_Parcels",
+                                    layer = "Abandoned_Property_Parcels",
+                                    stringsAsFactors = FALSE)
+
+#City Council data
+CityCouncilDistricts <- readOGR(dsn = "C:/Users/kost1/Documents/GitHub/Data-Viz-Final-Project/Data/City_Council_Districts",
+                                layer = "City_Council_Districts",
+                                stringsAsFactors = FALSE)
+
+#Cenus data
+census <- readOGR(dsn="C:/Users/kost1/Documents/GitHub/Data-Viz-Final-Project/Data/2010_Cenusdata",
+                  layer ='2010_CensusData',
+                  stringsAsFactors = FALSE)
 
 parksSpatial <- SpatialPointsDataFrame(coords = parksPoints[,c("Lon","Lat")], data = parksPoints,
                                        proj4string = CRS("+proj=longlat +datum=WGS84"))
 
-AbandonedPropertyParcels <- readOGR(dsn = ".C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject/data/Abandoned_Property_Parcels/", layer = "Abandoned_Property_Parcels", stringsAsFactors = FALSE)
-CityCouncilDistricts <- readOGR(dsn = "C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject/data/City_Council_Districts/", layer = "City_Council_Districts", stringsAsFactors = FALSE)
 
 AbandonedPropertyParcels.center <- SpatialPointsDataFrame(gCentroid(AbandonedPropertyParcels, byid=TRUE), 
                                                           AbandonedPropertyParcels@data, match.ID=FALSE)
-census <- readOGR(dsn="/Users/Samantha/Documents/R/Data Visualization",layer ='2010_CensusData', stringsAsFactors = FALSE)
-public_facilities <- read.csv('C:/Users/Samantha/Documents/R/Data Visualization/Public_Facilities (1).csv')
+
+public_facilities <- read.csv('C:/Users/kost1/Documents/GitHub/data-Viz-Final-Project/data/Public_Facilities.csv')
 facilities.spatial <- SpatialPointsDataFrame(coords = public_facilities[,c("Lon","Lat")], data = public_facilities, proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 
-street_lights <- read.csv("C:/Users/kost1/Documents/GitHub/Data-Viz-2018-Fall/FinalProject/Street_Lights.csv", stringsAsFactors = F)
+street_lights <- read.csv("C:/Users/kost1/Documents/GitHub/data-Viz-Final-Project/data/Street_Lights.csv", stringsAsFactors = F)
 street_lights[street_lights$Pole_Type %in% c(""," "),]$Pole_Type <- "Unknown"
 street_lights[street_lights$Service %in% c(""," "),]$Service <- "Unknown"
 street_lights$Inspect_Date2 <- as.Date(street_lights$Inspect_Date)
@@ -37,17 +57,20 @@ parksSpatial$popup <- paste("<b>",parksSpatial$Park_Name,"</b><br>",
                             "Address: ",parksSpatial$Address,sep ="")
 
 AbandonedPropertyParcels$popup <- paste("<b>",AbandonedPropertyParcels$Outcome_St,"</b><br>",
-                            "Address: ", AbandonedPropertyParcels$Address_Nu, AbandonedPropertyParcels$Street_Nam,AbandonedPropertyParcels$Suffix, "<br>", 
-                            "Structure Type: ", AbandonedPropertyParcels$Structures, "<br>", 
-                            "District: ", AbandonedPropertyParcels$Council_Di)
+                                        "Address: ", AbandonedPropertyParcels$Address_Nu, AbandonedPropertyParcels$Street_Nam,AbandonedPropertyParcels$Suffix, "<br>", 
+                                        "Structure Type: ", AbandonedPropertyParcels$Structures, "<br>", 
+                                        "District: ", AbandonedPropertyParcels$Council_Di)
 
 CityCouncilDistricts$popupCouncils <- paste("<b>","District: ", CityCouncilDistricts$OBJECTID, "</b><br>",
                                             "Council Member: ", CityCouncilDistricts$Council_Me)
 
 #Set color palettes
-pal <- colorFactor(palette = 'Set1', domain = parksSpatial$Park_Type)
-pal_A <- colorFactor(palette = "Accent", domain =AbandonedPropertyParcels$Outcome_St)
+palParks <- colorFactor(palette = 'Set1', domain = parksSpatial$Park_Type)
+palProperties <- colorFactor(palette = "Accent", domain =AbandonedPropertyParcels$Outcome_St)
+palCenus <- colorNumeric(palette = "RdYlBu", domain = census$SE_T002_01)
 
+facilitiesCheckboxChoices <- unique(facilities.spatial@data$POPL_TYPE)
+facilitiesCheckboxSelected <- unique(facilities.spatial@data$POPL_TYPE)
 
 #Create UI
 ui <- fluidPage(
@@ -93,43 +116,60 @@ ui <- fluidPage(
              tabPanel("Street Lights",
                       sidebarLayout(
                         sidebarPanel(
-                          dateRangeInput(inputId = "dates", label = "Date range", startview = "year",start="2010-01-01"),
-                          selectInput(inputId = "variable", label = "Variable:",choices = c("Pole_Type","Service"))
+                          dateRangeInput(inputId = "dates",
+                                         label = "Date range",
+                                         startview = "year",
+                                         start="2010-01-01"),
+                          selectInput(inputId = "variable",
+                                      label = "Variable:",
+                                      choices = c("Pole_Type","Service"))
                         ),#end sidebarPanel
                         
                         mainPanel(
                           tabsetPanel(
-                            tabPanel("Map",leafletOutput(outputId = "map"))
+                            tabPanel("Map",
+                                     leafletOutput(outputId = "map"))
                           )#end TabsetPanel          
                         )#end mainPanel
                       )#end sidebarLayout
              ),
-             tabPanel("Map of Public Facilities and Census Data"), 
-             "This tab shows a map of public facilities and census data in the Notre Dame Area. The intent of this analysis is to enable one to formulate a strategy on selecting
+             
+             tabPanel("Map of Public Facilities and Census data", 
+                      "This tab shows a map of public facilities and census data in the Notre Dame Area. The intent of this analysis is to enable one to formulate a strategy on selecting
              optimal locations for building public facilities in the future, as public facilities should be built in areas of high populations as the utilization rate should be
              higher. 
              When clicking on a colored area on the map, the name of the region will appear. Additionally, when clicking on a marker, the name of the type of facility will appear.
              The areas are colored by total population, therefore the area with the highest population will be colored blue, and the lowest population will be colored red (legend)
              shown to the bottom left. Lastly, one can select to only show specific facility types by selecting types at the top.",
-             sidebarLayout(
-               sidebarPanel(
-                 checkboxGroupInput("Type",
-                                    "Choose Facility Type",
-                                    choices = unique(facilities.spatial@data$POPL_TYPE),
-                                    selected = unique(facilities.spatial@data$POPL_TYPE))
+                      sidebarLayout(
+                        sidebarPanel(
+                          checkboxGroupInput("Type",
+                                             "Choose Facility Type",
+                                             choices = facilitiesCheckboxChoices,
+                                             selected = facilitiesCheckboxSelected)
+                        ),
+                        mainPanel()
+                      )
              ),
              tabPanel("Contact Us",
-                      textInput("firstContact", "First Name:"),
-                      textInput("lastContact", "Last Name:"),
-                      textInput("phoneContact", "Phone Number:"),
-                      textInput("emailContact", "Email Address:"),
-                      textInput("subjectContact", "Email Subject:"),
-                      textAreaInput("messageContact", "Enter your message"),
-                      actionButton("sendEmail", "Send Email")
+                      sidebarLayout(
+                        sidebarPanel(
+                          textInput("firstContact", "First Name:"),
+                          textInput("lastContact", "Last Name:"),
+                          textInput("phoneContact", "Phone Number:"),
+                          textInput("emailContact", "Email Address:"),
+                          textInput("subjectContact", "Email Subject:"),
+                          actionButton("sendEmail", "Send Email")
+                        ),
+                        mainPanel(
+                          "Enter your message below",
+                          aceEditor("messageContact")
+                        )
+                      )
              )
   )
 )
-)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -137,22 +177,21 @@ server <- function(input, output) {
   output$schoolsParksPlot <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      addCircleMarkers(data = parksSpatial, popup = ~popup, color=~pal(Park_Type), stroke = 0, fillOpacity=1, radius=4) %>%
-      addLegend(pal = pal, values = parksSpatial$Park_Type) %>%
+      addCircleMarkers(data = parksSpatial, popup = ~popup, color=~palParks(Park_Type), stroke = 0, fillOpacity=1, radius=4) %>%
+      addLegend(pal = palParks, values = parksSpatial$Park_Type) %>%
       addPolygons(data = schoolsSpatial, color = "blue", weight = 2, opacity = 0.6, popup = ~School)  
   })
   
   #create leaflet map - ASHLEY
-  output$AbandonedPropertyParcels <-
-      renderLeaflet({
-      leaflet()  %>%
+  output$AbandonedPropertyParcels <- renderLeaflet({
+    leaflet()  %>%
       addTiles()  %>%
       addProviderTiles(providers$OpenStreetMap) %>% #sets map design
-      addPolygons(data = CityCouncilDistricts, popup = ~popupCouncils, fillColor = ~pal_A(Dist), #creates district lines
+      addPolygons(data = CityCouncilDistricts, popup = ~popupCouncils, fillColor = ~palProperties(Dist), #creates district lines
                   weight = 2,
                   opacity = 1,
                   color = "white") %>% 
-      addPolylines(data = AbandonedPropertyParcels, popup = ~popup, color = ~pal_A(Outcome_St), fillOpacity = 1)
+      addPolylines(data = AbandonedPropertyParcels, popup = ~popup, color = ~palProperties(Outcome_St), fillOpacity = 1)
   })
   
   #create bar graph - ASHLEY
@@ -165,29 +204,51 @@ server <- function(input, output) {
       scale_fill_brewer(palette="Accent", na.value="grey") #same palette as palette for map
   })
   
-  
-  pal <- colorNumeric(palette = "RdYlBu", domain = census$SE_T002_01)
-  
   output$Map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       addMarkers(data = public_facilities[public_facilities$POPL_TYPE %in% input$Type,], 
                  popup = paste("Facility Type:",public_facilities$POPL_TYPE)) %>%
-      addPolygons(data = census,popup = paste("Area Name:",census$NAMELSAD), color = ~pal(SE_T002_01)) %>%
-      addLegend(data = census, position = "bottomright", pal = pal, 
+      addPolygons(data = census,popup = paste("Area Name:",census$NAMELSAD), color = ~palCenus(SE_T002_01)) %>%
+      addLegend(data = census, position = "bottomright", pal = palCenus, 
                 values = ~SE_T002_01, title = "Total Population", opacity = 1)  
+    
+  })
   
-})
-  
-  data <- eventReactive(input$dates,{
+  streetLights <- eventReactive(input$dates,{
     return(street_lights[street_lights$Inspect_Date2 >= input$dates[1]& street_lights$Inspect_Date2 <= input$dates[2],])
   })
+  
   output$map <-  renderLeaflet({
-    leaflet(data= data())%>%
+    leaflet(data = streetLights())%>%
       addTiles()%>%
       addMarkers(~Lon, ~Lat, popup = ~Pole_Num_1)
   })
   output$texty <- renderText(paste(input$dates[1]))
+  
+  observeEvent(input$sendEmail, {
+    
+    emailBody <- HTML(paste(input$messageContact, "\n
+The following message was sent by\n
+                        Name:", input$firstContact, input$lastContact, "\n",
+                        "Phone:", input$phoneContact, "\n",
+                        "Email:", input$emailContact
+    ))
+    
+    send.mail(from = "southbendcitymailbox@gmail.com",
+              to = "southbendcitymailbox@gmail.com",
+              subject = input$subjectContact,
+              body = emailBody,
+              html = TRUE,
+              smtp = list(host.name = "smtp.gmail.com",
+                          port = 465,
+                          user.name="southbendcitymailbox@gmail.com",
+                          passwd="0Z#cbf6NhQhr",
+                          ssl = TRUE),
+              authenticate = TRUE,
+              send = TRUE)
+  })
+  
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
